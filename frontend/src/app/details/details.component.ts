31d005a13777f1e17a72cc16e291fe6080706e6c
Sergi Biosca <serbidaw@gmail.com>
2022-10-17 15:47:07 +0200

    Check first details, in process filters

--- frontend/src/app/details/details.component.ts
+++ frontend/src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit} from '@angular/core';
+import { ProductService } from '../core/services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../core';
 
 @Component({
   selector: 'app-details',
@@ -7,8 +10,23 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class DetailsComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {
+  slug?: string = '';
+  product?: Product[];
+  constructor(
+    private ProductService: ProductService,
+    private ActivateRoute: ActivatedRoute
+  ) {
+    this.slug = this.ActivateRoute.snapshot.paramMap.get('slug') || '';
   }
-
+  
+  ngOnInit() {
+    this.product_details();
+  }
+  product_details() {
+    console.log(this.slug)
+    this.ProductService.get(this.slug).subscribe((data) => {
+      console.log(data);
+      this.product = data;
+    })
+  }
 }
