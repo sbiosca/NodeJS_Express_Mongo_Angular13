7af157d8b8793af50adbd13104c6ef3e387a6d16
rferritorro <rferritorro@gmail.com>
2022-10-18 15:52:23 +0200

    Frontend: Merge Home with Main

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,12 +6,9 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { CategoryComponent } from "./category/category.component";
 import { ProductComponent } from "./product/product.component";
 import { CarouselComponent } from "./carousel/carousel.component";
-<<<<<<< HEAD
 import { FiltersComponent } from "./filters/filters.component";
 import { NgxPaginationModule } from "ngx-pagination";
-=======
 import {CarouselModule} from 'primeng/carousel';
->>>>>>> home
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 //import { CarouselComponent } from "./carousel/carousel.component";
 
@@ -21,24 +18,18 @@ import {CarouselModule} from 'primeng/carousel';
         CommonModule,
         RouterModule,
         FontAwesomeModule,
-<<<<<<< HEAD
         NgxPaginationModule,
        // NgbModule
-=======
         CarouselModule
         // NgbModule
->>>>>>> home
         //CarouselModule
     ],
     declarations: [
         CategoryComponent,
         ProductComponent,
         CarouselComponent,
-<<<<<<< HEAD
         FiltersComponent,
-=======
         
->>>>>>> home
         // CarouselComponent
         
     ],
